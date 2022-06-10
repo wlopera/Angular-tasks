ce76e2019b8a748e37553649537f39588c6c512b
lopera.william@gmail.com <William>
2022-06-10 09:58:30 -0500

    agregar interface y servicio de borrar

--- src/app/tasks/list-tasks/list-tasks.component.ts
+++ src/app/tasks/list-tasks/list-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../interfaces/task';
 import { TasksService } from '../services/tasks.service';
 
 @Component({
@@ -10,7 +11,11 @@ export class ListTasksComponent {
     console.log(tasksService);
   }
 
-  get tasks(){
-    return this.tasksService.tasks
+  get tasks() {
+    return this.tasksService.tasks;
   }
+
+  delete = (task: Task) => {
+    this.tasksService.deleteTask(task.name);
+  };
 }
--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/task';
 
 @Injectable()
 export class TasksService {
   // ===> tasks: string[] = [];
-  tasks: Array<string> = ['Barrer', 'Trapear', 'Cocinar', 'leer'];
+  tasks: Array<Task> = [
+    {
+      name: 'Barrer',
+      complete: false,
+    },
+    {
+      name: 'Trapear',
+      complete: false,
+    },
+    {
+      name: 'Cocinar',
+      complete: false,
+    },
+    {
+      name: 'Leer',
+      complete: false,
+    },
+  ];
+
+  deleteTask = (taskName: string) => {
+    this.tasks = this.tasks.filter((task) => task.name !== taskName);
+  };
 }
